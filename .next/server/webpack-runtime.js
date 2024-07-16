4f439f380222ab10f4c4ee2a4e724588cc8e0fb5
Darsh Patel <darshanpatel1902@gmail.com>
2024-07-16 00:27:11 -0700

    updated path

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -30,7 +30,7 @@
 /******/ 		// Execute the module function
 /******/ 		var threw = true;
 /******/ 		try {
-/******/ 			__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 			__webpack_modules__[moduleId].call(module.exports, module, module.exports, __webpack_require__);
 /******/ 			threw = false;
 /******/ 		} finally {
 /******/ 			if(threw) delete __webpack_module_cache__[moduleId];
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("52fdcb4004de3b11")
+/******/ 		__webpack_require__.h = () => ("9b62574046175bb1")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c109856af8461793"; }
+/******/ 		__webpack_require__.h = function() { return "15321a7b19f82e5f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
