92e99c60c730f23ed65d7345f6eee73bab4ee12e
Darsh Patel <darshanpatel1902@gmail.com>
2024-07-16 12:27:54 -0700

    added tracking

--- app/(root)/template.tsx
+++ app/(root)/template.tsx
@@ -2,6 +2,7 @@
 
 import { AnimatePresence, motion, Spring } from 'framer-motion';
 import { usePathname } from 'next/navigation';
+import Script from 'next/script';
 
 export default function Template({ children }: { children: React.ReactNode }) {
     const transitionSpringPhysics: Spring = {
@@ -13,51 +14,66 @@ export default function Template({ children }: { children: React.ReactNode }) {
 
     const pathname = usePathname();
     return (
-        <AnimatePresence mode='wait'>
-            <motion.div key={pathname} className='overflow-hidden'>
-                <motion.div
-                    className='bg-gradient-to-r dark:from-indigo-500 dark:to-purple-500 !overflow-hidden'
-                    style={{
-                        position: 'fixed',
-                        width: '100vw',
-                        zIndex: 1000,
-                        top: 0,
-                    }}
-                    transition={transitionSpringPhysics}
-                    animate={{ height: '0vh' }}
-                    exit={{ height: '100vh' }}
-                />
+        <>
+            <Script
+                id="clarity-script"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+                        (function(c,l,a,r,i,t,y){
+                            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                        })(window, document, "clarity", "script", "n8elajyvxk");
+                    `,
+                }}
+            />
+            <AnimatePresence mode='wait'>
+                <motion.div key={pathname} className='overflow-hidden'>
+                    <motion.div
+                        className='bg-gradient-to-r dark:from-indigo-500 dark:to-purple-500 !overflow-hidden'
+                        style={{
+                            position: 'fixed',
+                            width: '100vw',
+                            zIndex: 1000,
+                            top: 0,
+                        }}
+                        transition={transitionSpringPhysics}
+                        animate={{ height: '0vh' }}
+                        exit={{ height: '100vh' }}
+                    />
 
-                <motion.div
-                    className='bg-gradient-to-r dark:from-indigo-500 dark:to-purple-500 !overflow-hidden'
-                    style={{
-                        position: 'fixed',
-                        width: '100vw',
-                        zIndex: 1000,
-                        bottom: 0,
-                    }}
-                    transition={transitionSpringPhysics}
-                    initial={{ height: '100vh' }}
-                    animate={{ height: '0vh', transition: { delay: 0.2 } }}
-                />
-                <motion.div
-                    initial={{
-                        opacity: 0,
-                        y: 20,
-                    }}
-                    animate={{
-                        opacity: 1,
-                        y: [20, -5, 0],
-                    }}
-                    transition={{
-                        duration: 0.5,
-                        ease: [0.4, 0.0, 0.2, 1],
-                        delay: 0.35,
-                    }}
-                >
-                    <div className='min-h-[85vh]'>{children}</div>
+                    <motion.div
+                        className='bg-gradient-to-r dark:from-indigo-500 dark:to-purple-500 !overflow-hidden'
+                        style={{
+                            position: 'fixed',
+                            width: '100vw',
+                            zIndex: 1000,
+                            bottom: 0,
+                        }}
+                        transition={transitionSpringPhysics}
+                        initial={{ height: '100vh' }}
+                        animate={{ height: '0vh', transition: { delay: 0.2 } }}
+                    />
+                    <motion.div
+                        initial={{
+                            opacity: 0,
+                            y: 20,
+                        }}
+                        animate={{
+                            opacity: 1,
+                            y: [20, -5, 0],
+                        }}
+                        transition={{
+                            duration: 0.5,
+                            ease: [0.4, 0.0, 0.2, 1],
+                            delay: 0.35,
+                        }}
+                    >
+                        <div className='min-h-[85vh]'>{children}</div>
+                    </motion.div>
                 </motion.div>
-            </motion.div>
-        </AnimatePresence>
+            </AnimatePresence>
+        </>
     );
 }
