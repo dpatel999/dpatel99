375556a423650f6d2d0b22315936d345b2968d4b
Darsh Patel <darshanpatel1902@gmail.com>
2024-07-15 21:38:18 -0700

    updated project data

--- utils/data.ts
+++ utils/data.ts
@@ -86,74 +86,74 @@ export const work = [
 - Ensured application security through the implementation of OAuth, JWT, and adherence to OWASP standards.
 - Collaborated with cross-functional teams to stay abreast of industry trends and emerging technologies.`,
     },
-    {
-        title: 'Full Stack Developer',
-        location: 'Understance, Vancouver, BC',
-        date: 'Mar 2021 - May 2023',
-        description: `- Developed and maintained high-performance eCommerce sites using Shopify Liquid and the Hydrogen framework.
-- Led the launch of multiple Shopify Plus stores, handling everything from theme development to custom app integration.
-- Designed and implemented robust Canvas apps with Dataverse backend and SharePoint connectors.
-- Spearheaded the development of a self-returns portal using React and Microsoft Power Automate, streamlining return processes.
-- Created custom Shopify apps and themes, leveraging Tailwind CSS and TypeScript for efficient, scalable solutions.
-- Automated workflows and business processes using cloud flows, enhancing operational efficiency.
-- Participated actively in agile ceremonies, including sprint planning, daily standups, and retrospectives.`,
-    },
-    {
-        title: 'Business Application Developer',
-        location: 'Understance, Vancouver, BC',
-        date: 'Mar 2021 - Present',
-        description: `- Developed and deployed business tools using the Power Platform, enhancing automation and efficiency across various departments.
-- Collaborated with Solution Strategists and Delivery Managers to gather requirements and provide technical suggestions.
-- Designed and developed end-to-end automation solutions using PowerApps, Power Automate, and RPA.
-- Integrated D2C tools to enhance customer experience and gather valuable data for business insights.
-- Built and maintained eCommerce sites using Shopify Liquid language and Admin/Storefront API.
-- Created a clothing brand with a Shopify Plus store from scratch, leveraging the Power Platform for backend operations.
-- Designed, configured, and implemented Canvas apps with Dataverse backend and SharePoint connectors.
-- Developed a scalable self-returns portal using React and Microsoft Power Automate, improving the returns process.
-- Built custom app templates and themes for a consistent look across the organization.
-- Developed cloud flows triggered based on schedule to automate business processes.
-- Created custom PCF controls using Power Platform CLI, TypeScript, and React for enhanced user interface designs.`,
-    },
-    {
-        title: 'Web Developer',
-        location: 'Pixel Architect, Kamloops, BC',
-        date: 'Feb 2020 - Mar 2020',
-        description: `- Developed professional websites from the ground up using WordPress, ensuring high-quality user experiences.
-- Implemented QA tests and documentation within an agile development workflow, guaranteeing reliable and bug-free deployments.
-- Collaborated with team members using online group management tools to report progress, milestones, and tasks.
-- Successfully deployed websites to staging for review and production, adhering to established timelines.
-- Contributed to product concepts by providing wireframes, mock-ups, and interactive prototypes, promoting efficient collaboration.
-- Worked closely with Government and Non-Profit organizations, tailoring solutions to meet specific requirements.`,
-    },
-    {
-        title: 'Software Developer',
-        location: 'BC Lottery Corporation, Kamloops, BC',
-        date: 'May 2019 - Jan 2020',
-        description: `- Developed mobile and web applications using ReactJS and React Native, simplifying the lottery purchase process.
-- Leveraged Amazon Web Services to prototype mobile applications, ensuring optimal performance and scalability.
-- Implemented agile management principles, leading sprint planning, standups, and retrospectives to ensure project success.
-- Followed a test-driven approach to coding application components, resulting in robust and high-quality software.
-- Collaborated with UI/UX team from Vancouver, making design decisions that enhanced user engagement and satisfaction.`,
-    },
-    {
-        title: 'Freelance Developer',
-        location: 'Various Clients',
-        date: 'Sep 2019 - Present',
-        description: `- Conducted comprehensive client meetings to understand website requirements and successfully deliver tailored solutions.
-- Acted as a primary communication point between clients and hosting companies, ensuring smooth coordination.
-- Designed and developed cross-platform native apps and web apps using React, including a highly functional food delivery app for Namar Canada.
-- Utilized Shopify and WordPress to build custom websites, incorporating plugins and filters to meet specific business needs.
-- Managed the design, maintenance, and hosting of LAMP websites on AWS, ensuring optimal performance and security.`,
-    },
-    {
-        title: 'Co-Founder',
-        location: 'CarGO Marketplace',
-        date: 'Jun 2019 - Feb 2020',
-        description: `- Co-founded an innovative startup aimed at solving marketplace challenges.
-- Developed an e-commerce application using React Native Framework, enabling users to buy and sell used goods with ease.
-- Leveraged Google Firebase to develop a robust backend for the mobile application.
-- Participated in all aspects of business development, from market research to operations, ensuring alignment with the company's vision.`,
-    },
+//     {
+//         title: 'Full Stack Developer',
+//         location: 'Understance, Vancouver, BC',
+//         date: 'Mar 2021 - May 2023',
+//         description: `- Developed and maintained high-performance eCommerce sites using Shopify Liquid and the Hydrogen framework.
+// - Led the launch of multiple Shopify Plus stores, handling everything from theme development to custom app integration.
+// - Designed and implemented robust Canvas apps with Dataverse backend and SharePoint connectors.
+// - Spearheaded the development of a self-returns portal using React and Microsoft Power Automate, streamlining return processes.
+// - Created custom Shopify apps and themes, leveraging Tailwind CSS and TypeScript for efficient, scalable solutions.
+// - Automated workflows and business processes using cloud flows, enhancing operational efficiency.
+// - Participated actively in agile ceremonies, including sprint planning, daily standups, and retrospectives.`,
+//     },
+//     {
+//         title: 'Business Application Developer',
+//         location: 'Understance, Vancouver, BC',
+//         date: 'Mar 2021 - Present',
+//         description: `- Developed and deployed business tools using the Power Platform, enhancing automation and efficiency across various departments.
+// - Collaborated with Solution Strategists and Delivery Managers to gather requirements and provide technical suggestions.
+// - Designed and developed end-to-end automation solutions using PowerApps, Power Automate, and RPA.
+// - Integrated D2C tools to enhance customer experience and gather valuable data for business insights.
+// - Built and maintained eCommerce sites using Shopify Liquid language and Admin/Storefront API.
+// - Created a clothing brand with a Shopify Plus store from scratch, leveraging the Power Platform for backend operations.
+// - Designed, configured, and implemented Canvas apps with Dataverse backend and SharePoint connectors.
+// - Developed a scalable self-returns portal using React and Microsoft Power Automate, improving the returns process.
+// - Built custom app templates and themes for a consistent look across the organization.
+// - Developed cloud flows triggered based on schedule to automate business processes.
+// - Created custom PCF controls using Power Platform CLI, TypeScript, and React for enhanced user interface designs.`,
+//     },
+//     {
+//         title: 'Web Developer',
+//         location: 'Pixel Architect, Kamloops, BC',
+//         date: 'Feb 2020 - Mar 2020',
+//         description: `- Developed professional websites from the ground up using WordPress, ensuring high-quality user experiences.
+// - Implemented QA tests and documentation within an agile development workflow, guaranteeing reliable and bug-free deployments.
+// - Collaborated with team members using online group management tools to report progress, milestones, and tasks.
+// - Successfully deployed websites to staging for review and production, adhering to established timelines.
+// - Contributed to product concepts by providing wireframes, mock-ups, and interactive prototypes, promoting efficient collaboration.
+// - Worked closely with Government and Non-Profit organizations, tailoring solutions to meet specific requirements.`,
+//     },
+//     {
+//         title: 'Software Developer',
+//         location: 'BC Lottery Corporation, Kamloops, BC',
+//         date: 'May 2019 - Jan 2020',
+//         description: `- Developed mobile and web applications using ReactJS and React Native, simplifying the lottery purchase process.
+// - Leveraged Amazon Web Services to prototype mobile applications, ensuring optimal performance and scalability.
+// - Implemented agile management principles, leading sprint planning, standups, and retrospectives to ensure project success.
+// - Followed a test-driven approach to coding application components, resulting in robust and high-quality software.
+// - Collaborated with UI/UX team from Vancouver, making design decisions that enhanced user engagement and satisfaction.`,
+//     },
+//     {
+//         title: 'Freelance Developer',
+//         location: 'Various Clients',
+//         date: 'Sep 2019 - Present',
+//         description: `- Conducted comprehensive client meetings to understand website requirements and successfully deliver tailored solutions.
+// - Acted as a primary communication point between clients and hosting companies, ensuring smooth coordination.
+// - Designed and developed cross-platform native apps and web apps using React, including a highly functional food delivery app for Namar Canada.
+// - Utilized Shopify and WordPress to build custom websites, incorporating plugins and filters to meet specific business needs.
+// - Managed the design, maintenance, and hosting of LAMP websites on AWS, ensuring optimal performance and security.`,
+//     },
+//     {
+//         title: 'Co-Founder',
+//         location: 'CarGO Marketplace',
+//         date: 'Jun 2019 - Feb 2020',
+//         description: `- Co-founded an innovative startup aimed at solving marketplace challenges.
+// - Developed an e-commerce application using React Native Framework, enabling users to buy and sell used goods with ease.
+// - Leveraged Google Firebase to develop a robust backend for the mobile application.
+// - Participated in all aspects of business development, from market research to operations, ensuring alignment with the company's vision.`,
+//     },
 ];
 export const projects = [
     {
@@ -164,73 +164,5 @@ export const projects = [
         hostedUrl: 'https://main.d2rt4o06eio0zv.amplifyapp.com/',
         association: 'Sleep Country',
         impact: 'Reduced page load times by 20%, increased user engagement by 25%, and improved eCommerce conversion rates by 15%.'
-    },
-    {
-        projectName: 'Full Stack Developer',
-        projectDesc: `Developed and maintained high-performance eCommerce sites using Shopify Liquid and the Hydrogen framework at Understance. Led the launch of multiple Shopify Plus stores, handling everything from theme development to custom app integration. Designed and implemented robust Canvas apps with Dataverse backend and SharePoint connectors. Spearheaded the development of a self-returns portal using React and Microsoft Power Automate, streamlining return processes. Created custom Shopify apps and themes, leveraging Tailwind CSS and TypeScript for efficient, scalable solutions. Automated workflows and business processes using cloud flows, enhancing operational efficiency. Participated actively in agile ceremonies, including sprint planning, daily standups, and retrospectives.`,
-        techstack: 'Shopify Liquid, Hydrogen, React, Microsoft Power Automate, Tailwind CSS, TypeScript',
-        workDone: 'Full Stack Development',
-        hostedUrl: 'https://main.d2rt4o06eio0zv.amplifyapp.com/',
-        association: 'Understance',
-        impact: 'Increased operational efficiency by 30% through workflow automation and improved customer return process efficiency by 40%.'
-    },
-    {
-        projectName: 'Business Application Developer',
-        projectDesc: `Developed and deployed business tools using the Power Platform at Understance, enhancing automation and efficiency across various departments. Collaborated with Solution Strategists and Delivery Managers to gather requirements and provide technical suggestions. Designed and developed end-to-end automation solutions using PowerApps, Power Automate, and RPA. Integrated D2C tools to enhance customer experience and gather valuable data for business insights. Built and maintained eCommerce sites using Shopify Liquid language and Admin/Storefront API. Created a clothing brand with a Shopify Plus store from scratch, leveraging the Power Platform for backend operations. Designed, configured, and implemented Canvas apps with Dataverse backend and SharePoint connectors. Developed a scalable self-returns portal using React and Microsoft Power Automate, improving the returns process. Built custom app templates and themes for a consistent look across the organization. Developed cloud flows triggered based on schedule to automate business processes. Created custom PCF controls using Power Platform CLI, TypeScript, and React for enhanced user interface designs.`,
-        techstack: 'Power Platform, PowerApps, Power Automate, RPA, Shopify Liquid, React, Dataverse, SharePoint',
-        workDone: 'Application Development',
-        hostedUrl: 'https://main.d2rt4o06eio0zv.amplifyapp.com/',
-        association: 'Understance',
-        impact: 'Enhanced departmental efficiency by 35% through automation solutions, improved data collection by 25%, and streamlined business processes.'
-    },
-    {
-        projectName: 'Web Developer',
-        projectDesc: `Developed professional websites from the ground up using WordPress at Pixel Architect, ensuring high-quality user experiences. Implemented QA tests and documentation within an agile development workflow, guaranteeing reliable and bug-free deployments. Collaborated with team members using online group management tools to report progress, milestones, and tasks. Successfully deployed websites to staging for review and production, adhering to established timelines. Contributed to product concepts by providing wireframes, mock-ups, and interactive prototypes, promoting efficient collaboration. Worked closely with Government and Non-Profit organizations, tailoring solutions to meet specific requirements.`,
-        techstack: 'WordPress, HTML, CSS, JavaScript, jQuery',
-        workDone: 'Web Development',
-        hostedUrl: 'https://main.d2rt4o06eio0zv.amplifyapp.com/',
-        association: 'Pixel Architect',
-        impact: 'Reduced development time by 25% through efficient project management, improved website performance by 20%, and enhanced customer satisfaction by 30%.'
-    },
-    {
-        projectName: 'Software Developer',
-        projectDesc: `Developed mobile and web applications using ReactJS and React Native at BC Lottery Corporation, simplifying the lottery purchase process. Leveraged Amazon Web Services to prototype mobile applications, ensuring optimal performance and scalability. Implemented agile management principles, leading sprint planning, standups, and retrospectives to ensure project success. Followed a test-driven approach to coding application components, resulting in robust and high-quality software. Collaborated with UI/UX team from Vancouver, making design decisions that enhanced user engagement and satisfaction.`,
-        techstack: 'ReactJS, React Native, AWS, TDD, Agile',
-        workDone: 'Software Development',
-        hostedUrl: 'https://main.d2rt4o06eio0zv.amplifyapp.com/',
-        association: 'BC Lottery Corporation',
-        impact: 'Improved application performance by 30%, enhanced user satisfaction by 20%, and streamlined the lottery purchase process.'
-    },
-    {
-        projectName: 'Freelance Developer',
-        projectDesc: `Conducted comprehensive client meetings to understand website requirements and successfully deliver tailored solutions. Acted as a primary communication point between clients and hosting companies, ensuring smooth coordination. Designed and developed cross-platform native apps and web apps using React, including a highly functional food delivery app for Namar Canada. Utilized Shopify and WordPress to build custom websites, incorporating plugins and filters to meet specific business needs. Managed the design, maintenance, and hosting of LAMP websites on AWS, ensuring optimal performance and security.`,
-        techstack: 'React, Shopify, WordPress, AWS, LAMP',
-        workDone: 'Freelance Development',
-        hostedUrl: 'https://main.d2rt4o06eio0zv.amplifyapp.com/',
-        association: 'Various Clients',
-        impact: 'Delivered 20+ successful projects, improved client satisfaction by 40%, and increased website performance by 25%.'
-    },
-    {
-        projectName: 'Co-Founder',
-        projectDesc: `Co-founded an innovative startup, CarGO Marketplace, aimed at solving marketplace challenges. Developed an e-commerce application using React Native Framework, enabling users to buy and sell used goods with ease. Leveraged Google Firebase to develop a robust backend for the mobile application. Participated in all aspects of business development, from market research to operations, ensuring alignment with the company’s vision.`,
-        techstack: 'React Native, Firebase, Google Cloud',
-        workDone: 'Startup Development',
-        hostedUrl: 'https://main.d2rt4o06eio0zv.amplifyapp.com/',
-        association: 'CarGO Marketplace',
-        impact: 'Grew user base to 10,000+ within 6 months, improved transaction completion rates by 35%, and achieved a 30% increase in user retention.'
-    },
-    {
-        projectName: 'Technical Projects',
-        projectDesc: `Various technical projects including:
-        - Power Platform Automation: Designed and implemented complex automation workflows using PowerApps and Power Automate to streamline business processes.
-        - Secure Web Application Development: Developed secure web applications using OAuth, JWT, and SSL/TLS, adhering to OWASP standards.
-        - Machine Learning Projects: Leveraged scikit-learn and TensorFlow to develop predictive models and data analysis tools for business insights.
-        - CI/CD Implementation: Created and managed CI/CD pipelines using Jenkins and GitHub Actions, automating deployments and improving development cycles.
-        - Cross-Platform Development: Developed robust cross-platform applications using React Native and React, enhancing user experience across devices.`,
-        techstack: 'PowerApps, Power Automate, OAuth, JWT, SSL/TLS, scikit-learn, TensorFlow, Jenkins, GitHub Actions, React Native, React',
-        workDone: 'Technical Development',
-        hostedUrl: 'https://main.d2rt4o06eio0zv.amplifyapp.com/',
-        association: 'Various',
-        impact: 'Increased process efficiency by 30%, improved application security, developed predictive models with 85% accuracy, and enhanced user experience across platforms.'
-    },
+    }
 ];
